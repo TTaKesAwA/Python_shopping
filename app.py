b3577f40e931374bd21e59da4450fa186ccee138
takesawa <t.takesawa.sys22@morijyobi.ac.jp>
2023-07-19 14:59:55 +0900

    登録機能の追加

--- app.py
+++ app.py
@@ -1,24 +1,43 @@
-from flask import Flask, render_template,request
-import random
-import db
+from flask import Flask, render_template, request, redirect ,url_for, session
+import db, string, random
+from datetime import timedelta
+
+
 
 
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return render_template('index.html')
 
-@app.route('/topmenuback')
-def topmenu_back():
-    return render_template('index.html')
+
+@app.route('/adminkari')
+def admin_kari():
+    return render_template('admin_topmenu.html')
+
+@app.route('/admintopmenuback')
+def admin_topmenu_back():
+    return render_template('admin_topmenu.html')
 
 @app.route('/foodlist')
 def food_list():
     food_list = db.select_all_foods()
     return render_template('food_list.html', food=food_list)
+
+@app.route('/foodeditinput')
+def food_edit_input():
+    return render_template('food_edit_input.html')
+
+@app.route('/foodedit', methods=['POST'])
+def food_edit():
+    id= request.form.get('id')
+    name= request.form.get('name')
+    make= request.form.get('make')
+    worth= request.form.get('worth')
+    stock= request.form.get('stock')
+    db.edit_food(id,name, make, worth, stock)
+    
+    return render_template('editsucsess.html')
 
 @app.route('/food_search', methods=['POST'])
 def food_search():
@@ -64,8 +83,92 @@ def food_search_input():
 
 
 
+
+#ユーザー関連
+
+
+@app.route('/', methods=['GET'])
+def index():
+    msg = request.args.get('msg')
+    
+    if msg == None:
+        return render_template('index.html')
+    else:
+        return render_template('index.html', msg = msg)
+    
+@app.route('/', methods=['POST'])
+def login():
+    user_name = request.form.get('username')
+    mail = request.form.get('mail')
+    password = request.form.get('password')
+    
+    if db.login(user_name, mail,password):
+        session['user'] = True #sessionにuserバリューにTrueを保存
+        # 下2行は操作しないとログインページに戻されるやつ
+        session.permanent = True
+        app.permanent_session_lifetime = timedelta(minutes=100)
+        return redirect(url_for('mypage'))
+    else:
+        error = 'ログインに失敗しました。'
+        input_data  = {
+            'user_name': user_name,
+            'mail': mail,
+            'password': password
+        }
+        return render_template('index.html', error = error, data = input_data)
+
+@app.route('/logout')
+def logout():
+    session.pop('user', None)
+    return redirect(url_for('index'))
+
+@app.route('/mypage', methods=['GET'])
+def mypage(): 
+    if 'user' in session:
+        return render_template('mypage.html', name='images/poteti.jpg')
+    else:
+        return redirect(url_for('index'))
+    
+@app.route('/register')
+def register_form():
+    return render_template('register.html')
+
+@app.route('/register_exe', methods=['POST'])
+def register_exe():
+    user_name = request.form.get('username')
+    mail = request.form.get('mail')
+    password = request.form.get('password')
+    
+    if user_name == '':
+        error = 'ユーザー名が未入力です。'
+        return render_template('register.html', error = error)
+    if password == '':
+        error = 'パスワードが未入力です。'
+        return render_template('register.html', error = error)
+    
+    count = db.insert_user(user_name, mail,password)
+    
+    if count == 1:
+        msg = '登録が完了しました。'
+        return render_template('index.html', msg=msg)
+    else:
+        error = '登録に失敗しました。'
+    return render_template('register.html', error=error)
+
+@app.route('/list')
+def sample_list():
+    touroku_list = db.select_all_touroku()
+    return render_template('list.html', touroku=touroku_list)
+
+@app.route('/sample_register')
+def sample_register():
+    return render_template('touroku.html')
+
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
 
 
 
--- db.py
+++ db.py
@@ -1,5 +1,5 @@
-import os
-import psycopg2
+import os, psycopg2, string, random, hashlib
+
 
 def get_connection():
     url = os.environ['DATABASE_URL']
@@ -10,7 +10,7 @@ def select_all_foods():
     connection = get_connection()
     cursor = connection.cursor()
     
-    sql = 'SELECT * FROM food'
+    sql = 'SELECT * FROM food ORDER BY id ASC;'
     
     cursor.execute(sql)
     rows = cursor.fetchall()
@@ -18,6 +18,21 @@ def select_all_foods():
     cursor.close()
     connection.close()
     return rows
+
+def edit_food(id,name,make,worth,stock):
+    connection = get_connection()
+    cursor = connection.cursor()
+    sql = 'UPDATE food SET name= %s, make= %s, worth= %s, stock= %s WHERE id = %s'
+    
+    cursor.execute(sql,(name,make,worth, stock,id))
+    
+    count = cursor.rowcount
+    
+    connection.commit()
+    cursor.close()
+    connection.close()
+    
+    return count
 
 def insert_food(name,make,worth,stock):
     connection = get_connection()
@@ -65,3 +80,67 @@ def delete_food(id):
     
     return count
 
+
+
+
+#ユーザー関連
+
+
+def get_salt():
+    charset =string.ascii_letters + string.digits
+    salt = ''.join(random.choices(charset, k=30))
+    return salt
+
+def get_hash(password, salt):
+    b_pw = bytes(password, 'utf-8')
+    b_salt = bytes(salt, 'utf-8')
+    hashed_password = hashlib.pbkdf2_hmac("sha256", b_pw, b_salt, 1000).hex()
+    return hashed_password
+    
+    
+def insert_user(user_name, mail,password):
+    sql = 'INSERT INTO ecuser VALUES(default, %s, %s, %s,%s)'
+    
+    salt = get_salt()
+    hashed_password = get_hash(password, salt)
+
+    try:
+        connection = get_connection()
+        cursor = connection.cursor()
+        
+        cursor.execute(sql, (user_name, mail,salt,hashed_password,))
+        count = cursor.rowcount #更新件数を取得
+        connection.commit()
+        
+    except psycopg2.DatabaseError:
+        count = 0
+    finally:
+        cursor.close()
+        connection.close()
+        
+    return count
+
+def login(user_name, password):
+    sql = 'SELECT hashed_password, salt FROM customer WHERE name = %s' #ユーザー名がに件取れる可能性があるから直すnmae=%sのとこ
+    flg = False
+
+    try :
+        connection = get_connection()
+        cursor = connection.cursor()
+        cursor.execute(sql, (user_name, ))
+        user = cursor.fetchone()
+
+        if user != None:
+            salt = user[1]
+
+            hashed_password = get_hash(password, salt)
+
+            if hashed_password == user[0]:
+                flg = True
+    except psycopg2.DatabaseError:
+        flg = False
+    finally :
+        cursor.close()
+        connection.close()
+    
+    return flg
